5340d58fcf914a3383ea7b908d39f0fc49883eb7
amyhsiao <52168338+amyhsiao@users.noreply.github.com>
2019-08-28 17:22:24 +0800

    updated code

--- kadane's_algorithm.cpp
+++ kadane's_algorithm.cpp
@@ -1,21 +1,17 @@
 int main()
 {
     hi;
+    int n;
     cin >> n;
     vector<int> a(n);
     rep(i, 0, n) cin >> a[i];
-    int res = a[0], l = 0, r = 1, m = -10000;
+    int t = a[0], r = 1, res = -10000;
     while (r < n)
     {
-        res += a[r];
-        if (a[r] > res)
-        {
-            res = a[r];
-            l = r;
-        }
+        t = max(t + a[r], a[r]);
+        res = max(t, res);
         r++;
-        m = max(m, res);
     }
-    cout << m << '\n';
+    cout << res << '\n';
     return 0;
 }
