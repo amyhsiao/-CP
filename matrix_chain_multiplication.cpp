dd195f2e8b7e6d8baa83c40d663189db178001e7
amyhsiao <52168338+amyhsiao@users.noreply.github.com>
2019-10-11 15:21:35 +0800

    minor changes in code

--- matrix_chain_multiplication.cpp
+++ matrix_chain_multiplication.cpp
@@ -4,15 +4,17 @@
     cin.tie(0);                       \
     cout.tie(0);
 #define vi vector<int>
+#define r first
+#define c second
 typedef long long ll;
 using namespace std;
 int main()
 {
     hi;
     int n;
-    vi a(50);
+    vector<pair<int, int>> a(50);
     ll dp[n][n];
-    rep(i, 0, n) cin >> a[i], dp[i][i] = 0, dp[i][i + 1] = 0;
+    rep(i, 0, n) cin >> a[i].r >> a[i].c, dp[i][i] = 0, dp[i][i + 1] = 0;
     for (int r = 1; r < n; r++)
     {
         for (int l = r - 2; l >= 0; l--)
@@ -20,7 +22,7 @@ int main()
             ll mn = 1000000000;
             for (int k = r - 1; k > l; k--)
             {
-                mn = min(mn, dp[l][k] + dp[k][r] + (a[r] * a[k] * a[l]));
+                mn = min(mn, dp[l][k] + dp[k][r] + (a[r].r * a[k].c * a[l].c));
             }
             dp[l][r] = mn;
         }
